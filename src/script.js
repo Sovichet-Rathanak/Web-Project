a5251d66028b72818715d1aa72653cb6b5cf2cd8
Menghuor <hourousa9@gmail.com>
2024-05-18 16:48:32 +0700

    update div to canvas

--- src/script.js
+++ src/script.js
@@ -1,6 +1,9 @@
 // Select the color picker element and the outerbox element
 const boardColorPicker = document.getElementById('TCP');
 const outerbox = document.querySelector('.outerbox');
+// Canvas
+const canvas = document.getElementById('mycanvas');
+const ctx = canvas.getContext("2d");
 
 // Add an event listener to the color picker input
 boardColorPicker.addEventListener('input', () => {
@@ -11,10 +14,15 @@ boardColorPicker.addEventListener('input', () => {
 //Getting user input from text field
 document.addEventListener('DOMContentLoaded', (event) => {
     const userInput = document.getElementById("Input");
-    const ledText = document.getElementById("led-text");
+    // const ledText = document.getElementById("led-text");
 
     // Event listener for input field
     userInput.addEventListener('input', () => {
-        ledText.textContent = userInput.value;
+        // ledText.textContent = userInput.value;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillText(userInput.value, 10, 80);
     });
 });
+
+
+
